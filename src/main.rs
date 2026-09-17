944ca9214ca50607e5e3e2b0a74dd3465b0f7dfa
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-09-17 12:35:49 +0500

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use rayon::prelude::* ;
+
+fn main() {
+    let res = (0..1_000_000)
+                            .into_par_iter()
+                            .map(|x| x * x)
+                            .sum::<u64>() ;
+
+    println!("{}", res) ;   // Out: 333332833333500000
+
+    let v_res = [0, 1, 3, 4, 5]
+                            .par_iter()
+                            .map(|x| x * x)
+                            .collect::<Vec<_>>() ;
+
+    println!("{:?}", v_res) ;
+}
